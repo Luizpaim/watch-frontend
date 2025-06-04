16ea0da54ba0c0ede4539759a20680da3d66ffd6
Luizpaim <luizhenriquesantospaim@gmail.com>
2025-06-04 09:30:58 -0300

    fix: websocket

--- src/composables/useSocket.ts
+++ src/composables/useSocket.ts
@@ -1,44 +1,48 @@
-import { io, type Socket } from "socket.io-client";
-import { ref, computed, watch } from "vue";
+import { io, type Socket } from "socket.io-client"
+import { ref, computed, watch } from "vue"
 
-let socket: Socket | null = null;
-const currentSocket = ref<Socket | null>(null);
+let socket: Socket | null = null
+const currentSocket = ref<Socket | null>(null)
 
 export const useSocket = () => {
-  const user = useCookie<UserType>("user");
-  const token = computed(() => user?.value?.accessToken);
-  const config = useRuntimeConfig();
-  const baseURL = config.public.apiGateway as string;
+    const user = useCookie<UserType>("user")
+    const token = computed(() => user?.value?.accessToken)
+    const config = useRuntimeConfig()
+    const baseURL = config.public.apiGateway as string
 
-  watch(
-    token,
-    (newToken) => {
-      if (socket) {
-        socket.disconnect();
-        socket = null;
-        currentSocket.value = null;
-      }
-      if (newToken) {
-        socket = io(baseURL, {
-          transports: ["websocket"],
-          withCredentials: true,
-          auth: { token: newToken },
-        });
-        currentSocket.value = socket;
-        socket.on("connect", () => true);
-        socket.on("disconnect", () => false);
-      }
-    },
-    { immediate: true },
-  );
+    watch(
+        token,
+        (newToken) => {
+            if (socket) {
+                socket.disconnect()
+                socket = null
+                currentSocket.value = null
+            }
+            if (newToken) {
+                socket = io(baseURL, {
+                    transports: ["websocket"],
+                    withCredentials: true,
+                    auth: { token: newToken },
+                })
+                currentSocket.value = socket
+                socket.on("connect", () => {
+                    console.info("Websocket connected")
+                })
+                socket.on("disconnect", () => {
+                    console.warn("Websocket desconnected")
+                })
+            }
+        },
+        { immediate: true }
+    )
 
-  const on = (event: string, callback: (...args: any[]) => void) => {
-    currentSocket.value?.on(event, callback);
-  };
+    const on = (event: string, callback: (...args: Record<string, any>[]) => void) => {
+        currentSocket.value?.on(event, callback)
+    }
 
-  const off = (event: string, callback: (...args: any[]) => void) => {
-    currentSocket.value?.off(event, callback);
-  };
+    const off = (event: string, callback: (...args: Record<string, any>[]) => void) => {
+        currentSocket.value?.off(event, callback)
+    }
 
-  return { socket: currentSocket, on, off };
-};
+    return { socket: currentSocket, on, off }
+}
